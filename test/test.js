a150e871828e275340fc6824af968f732838d016
amaan287 <amaanmirza287@gmail.com>
2024-12-15 14:19:14 +0530

    made test cases for signin and signup

--- test/test.js
+++ test/test.js
@@ -1,1 +1,20 @@
-import axios from 'axios';
+const axios = require('axios');
+const BACKEND_URL = "http://localhost:3000";
+
+describe('Authentication', () => {
+    test('User is able to signup', async () => {
+        const name = "John Doe";
+        const email = `john.doe.${Math.floor(Math.random() * 100000)}@example.com`;
+        const password = "12345678";
+        const response = await axios.post(`${BACKEND_URL}/api/v1/auth/register`, { name, email, password });
+        expect(response.status).toBe(200);
+    });
+    test("user is able to signin", async () => {
+        const name = "user2";
+        const email = `john.doe.${Math.floor(Math.random() * 100000)}@example.com`;
+        const password = "12345678";
+        await axios.post(`${BACKEND_URL}/api/v1/auth/register`, { name, email, password });
+        const response = await axios.post(`${BACKEND_URL}/api/v1/auth/signin`, { email, password });
+        expect(response.status).toBe(200);
+    })
+});
