fc7ca576819c9c99f37a44692170f1b8e26a3365
amaan287 <amaanmirza287@gmail.com>
2024-12-30 20:55:56 +0530

    review controller and routes made

--- backend/controller/review.controller.js
+++ backend/controller/review.controller.js
@@ -40,3 +40,85 @@ export const createReview = async (req, res, next) => {
         next(handleError(500, error));
     }
 }
+
+export const getReviews = async (req, res, next) => {
+    try {
+        const reviews = await prisma.review.findMany();
+        res.status(200).json({ message: "All reviews", data: reviews });
+
+    } catch (error) {
+        console.log("error getting reviews: ", error);
+        next(handleError(500, error));
+    }
+}
+
+export const getReviewById = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const review = await prisma.review.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        res.status(200).json({ message: "Review", data: review });
+
+    } catch (error) {
+        console.log("error getting review: ", error);
+        next(handleError(500, error));
+    }
+}
+
+
+export const updateReview = async (req, res, next) => {
+    const id = req.params.id;
+    const { rating,
+        reviewType,
+        role,
+        companyName,
+        responsibilities,
+        feedback,
+        hoursPerWeek,
+        salaryPerWeek } = req.body;
+    if (!rating || !reviewType || !companyName) {
+        return next(handleError(400, "Please provide all fields"));
+    }
+    try {
+        const reviewData = await prisma.review.update({
+            where: {
+                id: String(id)
+            },
+            data: {
+                rating,
+                reviewType,
+                role,
+                companyName,
+                responsibilities,
+                feedback,
+                hoursPerWeek,
+                salaryPerWeek
+
+            }
+        });
+        res.status(200).json({ message: "Review updated", data: reviewData });
+
+    } catch (error) {
+        console.log("error updating review: ", error);
+        next(handleError(500, error));
+    }
+}
+
+export const deleteReview = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        await prisma.review.delete({
+            where: {
+                id: String(id)
+            }
+        });
+        res.status(200).json({ message: "Review deleted" });
+
+    } catch (error) {
+        console.log("error deleting review: ", error);
+        next(handleError(500, error));
+    }
+}
--- backend/routes/review.routes.js
+++ backend/routes/review.routes.js
@@ -1,8 +1,13 @@
 import express from 'express';
 
-import { createReview } from '../controller/review.controller.js';
+import { createReview, getReviews, getReviewById, updateReview, deleteReview } from '../controller/review.controller.js';
 
 const router = express.Router();
 router.post('/create', createReview);
+router.get('/getReviews', getReviews);
+router.get('/getReviewById/:id', getReviewById);
+router.put('/updateReview/:id', updateReview);
+router.delete('/deleteReview/:id', deleteReview);
+
 
 export default router;
