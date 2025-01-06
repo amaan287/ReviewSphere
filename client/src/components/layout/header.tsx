a490f18cbab195bd42f6b9178b3a16586201c57c
amaan287 <amaanmirza287@gmail.com>
2025-01-06 09:41:37 +0530

    fixing header

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function FooterDock() {
   return (
-    <div className="fixed shadow-lg rounded-b-lg top-0 right-0 left-0 bg-white border-t border-gray-200 px-2 py-3  z-50">
-      <nav className="w-screen mx-auto">
-        <div className="flex items-center justify-between px-4">
+    <div className="fixed shadow-lg rounded-b-lg top-0 right-0 left-0 bg-white border-t border-gray-200 px-2 py-3 z-50">
+      <nav className="w-screen">
+        <div className="flex items-center justify-between px-6">
           <Link to="/" className="flex flex-col items-center">
             <h1 className="text-lg font-semibold">Heritage strokes</h1>
           </Link>
