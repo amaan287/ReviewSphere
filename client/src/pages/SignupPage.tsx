11d4df39fb15922e49d9d4c055c74422ba02b801
amaan287 <amaanmirza287@gmail.com>
2025-01-08 11:54:28 +0530

    error fixed

---
+++ client/src/pages/SignupPage.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "../components/SignupForm";
+
+export default function SignupPage(): JSX.Element {
+  return (
+    <div className="pt-8">
+      <SignupForm />
+    </div>
+  );
+}
